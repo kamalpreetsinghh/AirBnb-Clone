34274db3b5eb2faec84522528e4d5936f2ebd33c
Kamal Preet Singh <kamalpreetsingh025@gmail.com>
2023-06-04 12:04:24 -0400

    first commit

---
+++ app/components/Container.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Container: React.FC<Props> = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default Container;
